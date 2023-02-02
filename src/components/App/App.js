7ec6187c3928a7d81a6701a70b854ae977049c8c
Adam Hughes <adamjordanhughes@gmail.com>
2023-02-02 15:49:28 -0500

    Add and import the movie data info

---
+++ src/components/App/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import movieData from '../../assets/movieData'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>app is rendering</h1>
+    </div>
+  );
+}
+
+export default App;
