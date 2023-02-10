be0fa89ca5b51b9acbcdd6e1b0a1a89d4800cb29
Adam Hughes <adamjordanhughes@gmail.com>
2023-02-10 11:37:27 -0500

    Remove alt tag reference to image

--- src/components/MovieDetails/MovieDetails.js
+++ src/components/MovieDetails/MovieDetails.js
@@ -8,7 +8,7 @@ const MovieDetails = ({movie,returnHome}) => {
         <div className='movie-details-container'>
             <section className='main-info'>
                 <div className='image-section'>
-                    <img className='hero-image' src={movie.backdrop_path} alt='movie backdrop image'/>
+                    <img className='hero-image' src={movie.backdrop_path} alt='movie backdrop'/>
                     <p className='tagline'>{movie.tagline}</p>
                 </div>
                 <div className='main-details'>
@@ -27,12 +27,7 @@ const MovieDetails = ({movie,returnHome}) => {
                 <h3 className='budget'>Movie Budget: ${movie.budget.toLocaleString("en-US")}</h3>
                 <h3 className='revenue'>Movie Revenue: ${movie.revenue.toLocaleString("en-US")}</h3>
                 <h3 className='genres'>Genres: {String(movie.genres)} </h3>
-
-
             </section>
-            
-            
-
         </div>
     )
 }
