36df51ade49a3ad3307465a408344bdc782de1d3
Adam Hughes <adamjordanhughes@gmail.com>
2023-02-11 14:06:06 -0500

    Set up home intercept and fixture

--- cypress/e2e/home_spec.cy.js
+++ cypress/e2e/home_spec.cy.js
@@ -3,6 +3,6 @@ describe('Home page testing', () => {
     cy.visit('http://localhost:3000');
   });
   it('When a user visits the app, all movies should be displayed', () => {
-    cy.intercept
+    cy.intercept("GET",`https://rancid-tomatillos.herokuapp.com/api/v2/movies`, { fixture: 'movies.json' })
   })
 })
