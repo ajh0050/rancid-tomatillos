78befad42414c46b4046f1feb25d6af2d200fd6f
Adam Hughes <adamjordanhughes@gmail.com>
2023-02-11 13:40:37 -0500

    Set up cypress

---
+++ cypress/e2e/home_spec.cy.js
@@ -0,0 +1,8 @@
+describe('Home page testing', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000');
+  });
+  it('When a user visits the app, all movies should be displayed', () => {
+    cy.intercept
+  })
+})
